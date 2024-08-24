97d3bf4ee4f8ce5553f8da67ab8a586b1ab765ce
Evangelos Chocholis <105212289+Vangelis-Chocholis@users.noreply.github.com>
2024-08-24 20:03:36 +0300

    Update clustering_page.py

--- clustering_page.py
+++ clustering_page.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 #import plotly.colors
 import numpy as np
+from connect_to_database import load_from_db
 
 
 
@@ -73,10 +74,11 @@ def clustered_data_trend():
     tracks_clustered['cluster'] = tracks_clustered['cluster'].astype(str)
     #tracks_popularity = pd.read_csv('data/tracks_popularity_table.csv')
     try:
-        if 'cached_tracks_popularity_table' not in st.session_state:
-            raise KeyError("cached_tracks_popularity_table not found in session state")
-        tracks_popularity = st.session_state['cached_tracks_popularity_table']
-    
+        #if 'cached_tracks_popularity_table' not in st.session_state:
+         #   raise KeyError("cached_tracks_popularity_table not found in session state")
+        #tracks_popularity = st.session_state['cached_tracks_popularity_table']
+        sql = '''SELECT * FROM tracks_popularity_table '''
+        tracks_popularity = load_from_db(sql)
         tracks_data = pd.merge(tracks_table, tracks_clustered, on='track_id')
         tracks_data = pd.merge(tracks_data, tracks_popularity, on='track_id')
         # group by date and cluster aggregating the mean popularity
